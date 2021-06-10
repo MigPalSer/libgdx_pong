e8521952d6711d94f4983ab22a9bf2b5148af439
mps <migpalser@gmail.com>
2021-06-10 16:44:59 +0200

    create project

---
+++ desktop/src/com/migpalser/pong/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.migpalser.pong.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.migpalser.pong.PongMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PongMain(), config);
+	}
+}
